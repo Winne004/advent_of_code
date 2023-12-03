7cc1bb9bd4b7a82ff163473ade1230ddc5fa435a
Jeremy Winchester <Jeremy.c.Winchester@gmail.com>
2023-12-03 15:14:23 +0000

    added main func

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -7,11 +7,16 @@ def find_first_and_last_int(code: str) -> int:
     )
 
 
-filepath = "day_1/input.txt"
-calibration_document = open(filepath, "r")
+def main():
+    filepath = "day_1/input.txt"
+    calibration_document = open(filepath, "r")
 
-res = 0
-for line in calibration_document:
-    res += find_first_and_last_int(line)
+    res = 0
+    for line in calibration_document:
+        res += find_first_and_last_int(line)
 
-print(res)
+    return res
+
+
+if __name__ == "__main__":
+    main()
