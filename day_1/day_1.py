fde8c3283683a731579dd2f82bdc8c9d00fb4e3b
Jeremy Winchester <jeremy.c.winchester@gmail.com>
2023-12-08 16:13:43 +0000

    added dictionary to FindNumbersAsString class

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,3 +1,16 @@
+class FindNumbersAsStrings:
+    numbers_as_words = {
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
 def find_first_and_last_int(code: str) -> int:
     list_of_numbers = [char for char in code if char.isdigit()]
     return (
